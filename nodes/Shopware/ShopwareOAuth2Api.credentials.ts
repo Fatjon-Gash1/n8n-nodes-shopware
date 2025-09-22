d567919f9a77d7d048371b28f23e0c8b29d68899
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-09-22 16:58:24 +0200

    Create "OAuth2" credentials class with access token handling

---
+++ nodes/Shopware/ShopwareOAuth2Api.credentials.ts
@@ -0,0 +1,88 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	ICredentialType,
+	IHttpRequestHelper,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class ShopwareOAuth2Api implements ICredentialType {
+	name = 'shopwareOAuth2Api';
+
+	displayName = 'Shopware OAuth2 API';
+
+	documentationUrl = 'https://docs.shopware.com/en/shopware-6-en/settings/system/integrationen';
+
+	icon = { light: 'file:shopware.png', dark: 'file:shopware.png' } as const;
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Access Token',
+			name: 'accessToken',
+			type: 'hidden',
+
+			typeOptions: {
+				expirable: true,
+			},
+			default: '',
+		},
+		{
+			displayName: 'URL',
+			name: 'url',
+			type: 'string',
+			required: true,
+			default: '',
+		},
+		{
+			displayName: 'Client ID',
+			name: 'clientId',
+			type: 'string',
+			required: true,
+			default: '',
+		},
+		{
+			displayName: 'Client Secret',
+			name: 'clientSecret',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			required: true,
+			default: '',
+		},
+	];
+
+	async preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject) {
+		const url = credentials.url as string;
+		const { access_token } = (await this.helpers.httpRequest({
+			method: 'POST',
+			url: `${url.endsWith('/') ? url.slice(0, -1) : url}/api/oauth/token`,
+			body: {
+				client_id: credentials.clientId,
+				client_secret: credentials.clientSecret,
+				grant_type: 'client_credentials',
+			},
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		})) as { access_token: string };
+		return { accessToken: access_token };
+	}
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '=Bearer {{$credentials.accessToken}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials?.url}}',
+			url: '/api/_info/version',
+		},
+	};
+}
