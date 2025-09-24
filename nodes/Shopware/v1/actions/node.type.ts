01d0cdb579b3cf118739cdcf22605bd67cde2100
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-09-24 10:04:08 +0200

    fix: Narrrow down operation types

--- nodes/Shopware/v1/actions/node.type.ts
+++ nodes/Shopware/v1/actions/node.type.ts
@@ -1,7 +1,8 @@
 import type { AllEntities } from 'n8n-workflow';
 
 type NodeMap = {
-	product: 'create' | 'upsert' | 'deleteRecord' | 'get' | 'getMany' | 'search' | 'update';
+	// product: 'create' | 'upsert' | 'deleteRecord' | 'get' | 'getMany' | 'search' | 'update';
+	product: 'getMany';
 };
 
 export type ShopwareType = AllEntities<NodeMap>;
