f439b36ef7b85a54d6b713d9c9c030f45085268f
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-09-30 15:08:18 +0200

    feat: Add the new operations in the resource description

--- nodes/Shopware/v1/actions/product/Product.resource.ts
+++ nodes/Shopware/v1/actions/product/Product.resource.ts
@@ -1,8 +1,12 @@
 import type { INodeProperties } from 'n8n-workflow';
 
+import * as create from './create.operation';
+import * as deleteProduct from './deleteProduct.operation';
+import * as get from './get.operation';
 import * as getMany from './getMany.operation';
+import * as update from './update.operation';
 
-export { getMany };
+export { create, deleteProduct, get, getMany, update };
 
 export const description: INodeProperties[] = [
 	{
@@ -12,18 +16,46 @@ export const description: INodeProperties[] = [
 		noDataExpression: true,
 		options: [
 			{
+				name: 'Create',
+				value: 'create',
+				description: 'Create a product',
+				action: 'Create a product',
+			},
+			{
+				name: 'Delete',
+				value: 'deleteProduct',
+				description: 'Delete a product',
+				action: 'Delete a product',
+			},
+			{
+				name: 'Get',
+				value: 'get',
+				description: 'Retrieve a product',
+				action: 'Get a product',
+			},
+			{
 				name: 'Get Many',
 				value: 'getMany',
 				description: 'Retrieve many products',
 				action: 'Get many products',
 			},
+			{
+				name: 'Update',
+				value: 'update',
+				description: 'Update a product',
+				action: 'Update a product',
+			},
 		],
-		default: 'getMany',
+		default: 'get',
 		displayOptions: {
 			show: {
 				resource: ['product'],
 			},
 		},
 	},
+	...create.description,
+	...deleteProduct.description,
+	...get.description,
 	...getMany.description,
+	...update.description,
 ];
