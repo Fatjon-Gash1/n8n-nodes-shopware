1dc64a926946281eac409409b47fbeb281409ade
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-09-30 22:53:43 +0200

    chore: Add customer resource type with known operations

--- nodes/Shopware/v1/actions/node.type.ts
+++ nodes/Shopware/v1/actions/node.type.ts
@@ -2,6 +2,7 @@ import type { AllEntities } from 'n8n-workflow';
 
 type NodeMap = {
 	product: 'create' | 'deleteProduct' | 'get' | 'getMany' | 'update';
+	customer: 'get' | 'getMany';
 };
 
 export type ShopwareType = AllEntities<NodeMap>;
