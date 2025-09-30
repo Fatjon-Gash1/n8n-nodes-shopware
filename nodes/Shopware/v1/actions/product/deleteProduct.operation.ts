7787090e30a387b232d48b166228f85124cd0d7b
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-09-30 15:06:17 +0200

    feat: Add "deleteProduct" operation for product resource

---
+++ nodes/Shopware/v1/actions/product/deleteProduct.operation.ts
@@ -0,0 +1,65 @@
+import {
+	type INodeExecutionData,
+	type INodeProperties,
+	type IExecuteFunctions,
+	type JsonObject,
+	NodeApiError,
+	updateDisplayOptions,
+} from 'n8n-workflow';
+import { apiRequest } from '../../transport';
+import { wrapData } from '../../helpers/utils';
+
+const properties: INodeProperties[] = [
+	{
+		displayName: 'Product ID',
+		name: 'id',
+		type: 'string',
+		default: '',
+		placeholder: 'e.g. 2a88d9b59d474...',
+		required: true,
+		description:
+			'ID of the product to delete. You can find the ID in the URL when viewing the product in Shopware Admin (e.g. https://your-domain.com/admin#/sw/product/detail/&lt;productId&gt;).',
+	},
+];
+
+const displayOptions = {
+	show: {
+		resource: ['product'],
+		operation: ['deleteProduct'],
+	},
+};
+
+export const description = updateDisplayOptions(displayOptions, properties);
+
+export async function execute(
+	this: IExecuteFunctions,
+	items: INodeExecutionData[],
+): Promise<INodeExecutionData[]> {
+	const returnData: INodeExecutionData[] = [];
+
+	for (let i = 0; i < items.length; i++) {
+		try {
+			const id = this.getNodeParameter('id', i) as string;
+
+			await apiRequest.call(this, 'DELETE', `/product/${id}`);
+
+			const executionData = this.helpers.constructExecutionMetaData(
+				wrapData({ success: true, id, message: `Product ${id} deleted successfully` }),
+				{
+					itemData: { item: i },
+				},
+			);
+
+			returnData.push(...executionData);
+		} catch (error) {
+			if (this.continueOnFail()) {
+				returnData.push({ json: { error: error.message } });
+				continue;
+			}
+
+			throw new NodeApiError(this.getNode(), error as JsonObject);
+		}
+	}
+
+	return returnData;
+}
