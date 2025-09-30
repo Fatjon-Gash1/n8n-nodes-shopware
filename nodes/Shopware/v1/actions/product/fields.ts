8c466b4de85a676d8bf3f7ca968accef987610a9
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-09-30 14:59:13 +0200

    feat: Add product transport fields

---
+++ nodes/Shopware/v1/actions/product/fields.ts
@@ -0,0 +1,35 @@
+export const productFields = [
+	'id',
+	'parentId',
+	'manufacturerId',
+	'active',
+	'displayGroup',
+	'ratingAverage',
+	'weight',
+	'width',
+	'height',
+	'length',
+	'productNumber',
+	'name',
+	'description',
+	'available',
+	'availableStock',
+	'stock',
+	'price',
+	'categoryIds',
+	'ean',
+	'createdAt',
+	'updatedAt',
+];
+
+export const currencyFields = ['id', 'name', 'factor', 'isoCode'];
+
+export const productOptionFields = ['id', 'name'];
+
+export const currencyOptionFields = ['id', 'isoCode'];
+
+export const taxOptionFields = ['id', 'name', 'taxRate'];
+
+export const categoryOptionFields = ['id', 'name'];
+
+export const salesChannelOptionFields = ['id', 'name'];
