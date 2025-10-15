54c390b52e45cdaeebb1b1584c8c81a6d1132da1
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-10-15 22:25:05 +0200

    feat: Add category resource API fields and types

---
+++ nodes/Shopware/v1/actions/category/fields.ts
@@ -0,0 +1,17 @@
+export const categoryFields = [
+	'id',
+    'parentId',
+    'mediaId',
+    'breadcrumb',
+    'level',
+    'path',
+    'childCount',
+    'type',
+    'visible',
+    'active',
+    'visibleChildCount',
+    'name',
+    'description',
+    'keywords',
+    'createdAt'
+]
