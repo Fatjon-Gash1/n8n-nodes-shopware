2e3efbb3313af8b564635db3e8d5b14d80afa588
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2025-10-15 22:24:04 +0200

    feat: Add category resource "get" operation

---
+++ nodes/Shopware/v1/actions/category/get.operation.ts
@@ -0,0 +1,98 @@
+import {
+	type INodeExecutionData,
+	type INodeProperties,
+	type IExecuteFunctions,
+	type JsonObject,
+	NodeApiError,
+	updateDisplayOptions,
+} from 'n8n-workflow';
+import { apiRequest } from '../../transport';
+import { categoryFields } from './fields';
+import { wrapData } from '../../helpers/utils';
+
+const properties: INodeProperties[] = [
+	{
+		displayName: 'Category ID',
+		name: 'id',
+		type: 'string',
+		default: '',
+		placeholder: 'e.g. 2a88d9b59d474...',
+		required: true,
+		description:
+			'ID of the category to get. You can find the ID in the URL when viewing the category in Shopware Admin (e.g. https://your-domain.com/admin#/sw/category/detail/&lt;categoryId&gt;).',
+	},
+	{
+		displayName: 'Filters',
+		name: 'filters',
+		type: 'collection',
+		placeholder: 'Add Filter',
+		default: {},
+		options: [
+			{
+				displayName: 'Fields',
+				name: 'fields',
+				type: 'string',
+				default: '',
+				description:
+					'Fields the category will return, formatted as a string of comma-separated values. By default all the fields are returned.',
+			},
+		]
+	}
+];
+
+const displayOptions = {
+	show: {
+		resource: ['category'],
+		operation: ['get'],
+	},
+};
+
+export const description = updateDisplayOptions(displayOptions, properties);
+
+export async function execute(
+	this: IExecuteFunctions,
+	items: INodeExecutionData[],
+): Promise<INodeExecutionData[]> {
+	const returnData: INodeExecutionData[] = [];
+
+	for (let i = 0; i < items.length; i++) {
+		try {
+			let fields = categoryFields;
+
+			const id = this.getNodeParameter('id', i) as string;
+			const filters = this.getNodeParameter('filters', i);
+
+			const shrinkedFields = filters.fields;
+
+			if (shrinkedFields) {
+				fields = (shrinkedFields as string).split(',').map((field) => field.trim());
+			}
+
+			const body = {
+				fields,
+				includes: {
+					category: fields,
+				},
+				filter: [{ type: 'equals', field: 'id', value: id }],
+			};
+
+			const response = await apiRequest.call(this, 'POST', `/search/category`, body);
+
+			const executionData = this.helpers.constructExecutionMetaData(wrapData(response.data), {
+				itemData: { item: i },
+			});
+
+			returnData.push(...executionData);
+		} catch (error) {
+			if (this.continueOnFail()) {
+				returnData.push({ json: { error: error.message } });
+				continue;
+			}
+
+			throw new NodeApiError(this.getNode(), error as JsonObject);
+		}
+	}
+
+	return returnData;
+}
+
